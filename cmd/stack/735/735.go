1d542f54680a861bfb62ce608b76d230222a52fa
mikha <ya.mikushoff@gmail.com>
2022-08-01 18:13:54 +0300

    implement 735

--- cmd/stack/735/735.go
+++ cmd/stack/735/735.go
@@ -1,33 +1,46 @@
 package _735
 
-func handleCollision(ast int, stack *[]int) {
-	var getSize func() int
-	getSize = func() int {
-		return len(*stack) - 1
-	}
-	if ast > 0 {
-		*stack = append(*stack, ast)
-		return
-	}
-	for len(*stack) > 0 && (*stack)[getSize()] > 0 && -ast > (*stack)[getSize()] {
-		*stack = (*stack)[:getSize()]
-	}
+type Stack []int
 
-	if len(*stack) <= 0 || (*stack)[getSize()] < 0 {
-		*stack = append(*stack, ast)
+func (s *Stack) Pop() {
+	*s = (*s)[:(*s).Size()-1]
+}
+
+func (s *Stack) Push(value int) {
+	*s = append(*s, value)
+}
+
+func (s *Stack) Size() int {
+	return len(*s)
+}
+
+func (s *Stack) Last() int {
+	return (*s)[(*s).Size()-1]
+}
+
+func handleCollision(ast int, stack *Stack) {
+	if ast > 0 {
+		(*stack).Push(ast)
 		return
 	}
-	if (*stack)[getSize()] == -ast {
-		*stack = (*stack)[:getSize()]
+	for (*stack).Size() > 0 && (*stack).Last() > 0 && -ast > (*stack).Last() {
+		(*stack).Pop()
+	}
+	if (*stack).Size() <= 0 || (*stack).Last() < 0 {
+		(*stack).Push(ast)
+		return
+	}
+	if (*stack).Last() == -ast {
+		(*stack).Pop()
 	}
 }
 
 func asteroidCollision(asteroids []int) []int {
-	stack := make([]int, 0)
+	stack := &Stack{}
 
 	for _, ast := range asteroids {
-		handleCollision(ast, &stack)
+		handleCollision(ast, stack)
 	}
 
-	return stack
+	return *stack
 }
--- cmd/stack/735/735_test.go
+++ cmd/stack/735/735_test.go
@@ -11,3 +11,13 @@ func TestName(t *testing.T) {
 	assert.True(t, reflect.DeepEqual([]int{}, asteroidCollision([]int{8, -8})))
 	assert.True(t, reflect.DeepEqual([]int{10}, asteroidCollision([]int{10, 2 - 5})))
 }
+
+func TestStack_Last(t *testing.T) {
+	stack := &Stack{}
+	stack.Push(10)
+	assert.Equal(t, 1, stack.Size())
+	assert.Equal(t, 10, stack.Last())
+	stack.Pop()
+	assert.Equal(t, 0, stack.Size())
+	//assert.Equal(t, 10, stack.Last())
+}
