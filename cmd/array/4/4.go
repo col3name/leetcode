761a2defba68a8f5adf432452c157106e4811c9e
mikha <ya.mikushoff@gmail.com>
2022-08-02 04:31:52 +0300

    optimize 4

--- cmd/array/4/4.go
+++ cmd/array/4/4.go
@@ -4,17 +4,11 @@ import "math"
 
 func maxProfit(prices []int) int {
 	min := math.MaxInt32
-	index := 0
+	profit := 0
 	for i, price := range prices {
 		if price < min {
 			min = price
-			index = i
 		}
-	}
-
-	profit := 0
-
-	for i := index; i < len(prices); i++ {
 		if prices[i]-min > profit {
 			profit = prices[i] - min
 		}
