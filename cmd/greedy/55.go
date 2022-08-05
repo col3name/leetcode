5fa314e6d28fc6904b651f8416f281bdf0b77895
mikha <ya.mikushoff@gmail.com>
2022-08-05 15:57:51 +0300

    implement 55

---
+++ cmd/greedy/55.go
@@ -0,0 +1,15 @@
+package greedy
+
+func canJump(nums []int) bool {
+	max := nums[0]
+	for i := 1; i < len(nums); i++ {
+		if i > max {
+			return false
+		}
+
+		if max < i+nums[i] {
+			max = i + nums[i]
+		}
+	}
+	return true
+}
