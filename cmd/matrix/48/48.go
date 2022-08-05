33f540d63fd52610792c9ad8a0ea47cddaaaca88
mikha <ya.mikushoff@gmail.com>
2022-08-05 21:17:17 +0300

    implement 48

---
+++ cmd/matrix/48/48.go
@@ -0,0 +1,15 @@
+package _48
+
+func rotate(matrix [][]int) {
+	n := len(matrix)
+	var temp int
+	for i := 0; i < (n+1)/2; i++ {
+		for j := 0; j < n/2; j++ {
+			temp = matrix[n-1-j][i]
+			matrix[n-1-j][i] = matrix[n-1-i][n-j-1]
+			matrix[n-1-i][n-j-1] = matrix[j][n-1-i]
+			matrix[j][n-i-1] = matrix[i][j]
+			matrix[i][j] = temp
+		}
+	}
+}
