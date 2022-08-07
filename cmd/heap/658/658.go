9433f152c8434dfbfffa3a0219233682b1f2294a
mikha <ya.mikushoff@gmail.com>
2022-08-08 00:59:04 +0300

    implement 658 version 2

--- cmd/heap/658/658.go
+++ cmd/heap/658/658.go
@@ -63,3 +63,17 @@ func findClosestElements(arr []int, k int, x int) []int {
 	sort.Ints(result)
 	return result
 }
+
+func version2(arr []int, k int, x int) []int {
+	left, right := 0, len(arr)-k
+	for left < right {
+		mid := (left + right) / 2
+		if x-arr[mid] > arr[mid+k]-x {
+			left = mid + 1
+		} else {
+			right = mid
+		}
+
+	}
+	return arr[left : left+k]
+}
