e9fea4e56e263c5a5c9a2397a0c9484e93ca7d4f
mikha <ya.mikushoff@gmail.com>
2022-08-11 01:16:53 +0300

    implement 108. Convert Sorted Array to Binary Search Tree

---
+++ cmd/tree/bst/108/108.go
@@ -0,0 +1,18 @@
+package _108
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func sortedArrayToBST(nums []int) *TreeNode {
+	if len(nums) == 0 {
+		return nil
+	}
+	return &TreeNode{
+		Val:   nums[len(nums)/2],
+		Left:  sortedArrayToBST(nums[:len(nums)/2]),
+		Right: sortedArrayToBST(nums[len(nums)/2+1:]),
+	}
+}
