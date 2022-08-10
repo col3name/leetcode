14505732e175eecfb5c71e23f8acc610f9ddf82d
mikha <ya.mikushoff@gmail.com>
2022-08-11 01:01:29 +0300

    implement 449. Serialize and Deserialize BST

---
+++ cmd/tree/bt/449/449.go
@@ -0,0 +1,44 @@
+package _449
+
+import (
+	"strconv"
+	"strings"
+)
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+type Codec struct {
+}
+
+func Constructor() Codec {
+	return Codec{}
+}
+
+func (c *Codec) serialize(root *TreeNode) string {
+	if root == nil {
+		return "nil,"
+	}
+	return strconv.Itoa(root.Val) + "," + c.serialize(root.Left) + c.serialize(root.Right)
+}
+
+func (c *Codec) deserialize(data string) *TreeNode {
+	array := strings.Split(data, ",")
+	var decodeTree func() *TreeNode
+	decodeTree = func() *TreeNode {
+		if array[0] == "nil" {
+			array = array[1:]
+			return nil
+		}
+		number, _ := strconv.Atoi(array[0])
+		array = array[1:]
+		root := &TreeNode{number, nil, nil}
+		root.Left = decodeTree()
+		root.Right = decodeTree()
+		return root
+	}
+
+	return decodeTree()
+}
