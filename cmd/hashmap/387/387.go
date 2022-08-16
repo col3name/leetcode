1a66a4d5d3914248a714d132037283f23889a232
mikha <ya.mikushoff@gmail.com>
2022-08-16 13:17:01 +0300

    optimize 387. First Unique Character in a String

--- cmd/hashmap/387/387.go
+++ cmd/hashmap/387/387.go
@@ -1,12 +1,19 @@
 package _387
 
 func firstUniqChar(s string) int {
-	h := map[byte]int{}
+	h := [26]int{}
+	for i := 0; i < 26; i++ {
+		h[i] = -1
+	}
 	for _, char := range []byte(s) {
-		h[char]++
+		h[char-byte('a')]++
 	}
 	for i, char := range []byte(s) {
-		if h[char] == 1 {
+		i2 := h[char-byte('a')]
+		if i2 == -1 {
+			continue
+		}
+		if i2 == 1 {
 			return i
 		}
 	}
