6a3b7194d8dc144a8cbf71032de8f7cfc1597a1e
mikha <ya.mikushoff@gmail.com>
2022-08-17 11:54:14 +0300

    implement 31. Next Permutation

---
+++ cmd/backtracking/31/31.go
@@ -0,0 +1,22 @@
+package _31
+
+func nextPermutation(nums []int) {
+	i := len(nums) - 2
+	for i >= 0 && nums[i+1] <= nums[i] {
+		i--
+	}
+	if i >= 0 {
+		j := len(nums) - 1
+		for nums[j] <= nums[i] {
+			j--
+		}
+		nums[i], nums[j] = nums[j], nums[i]
+	}
+	i++
+	j := len(nums) - 1
+	for i < j {
+		nums[i], nums[j] = nums[j], nums[i]
+		i++
+		j--
+	}
+}
