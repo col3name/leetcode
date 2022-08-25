6d71abd15a5500b6fad5029933fdd839e3554572
mikha <ya.mikushoff@gmail.com>
2022-08-25 23:42:33 +0300

    refactor 383. Ransom Note

--- cmd/hashmap/383/383.go
+++ cmd/hashmap/383/383.go
@@ -3,20 +3,29 @@ package _383
 func canConstruct(ransomNote string, magazine string) bool {
 	memory := make(map[byte]int, 0)
 	for _, ch := range []byte(magazine) {
-		if _, ok := memory[ch]; ok {
-			memory[ch]++
-		} else {
-			memory[ch] = 1
-		}
+		memory[ch]++
 	}
 	for _, ch := range []byte(ransomNote) {
 		if _, ok := memory[ch]; !ok {
 			return false
-		} else {
-			memory[ch]--
-			if memory[ch] <= 0 {
-				delete(memory, ch)
-			}
+		}
+		memory[ch]--
+		if memory[ch] <= 0 {
+			delete(memory, ch)
+		}
+	}
+	return true
+}
+
+func canConstruct2(ransomNote string, magazine string) bool {
+	memory := make([]rune, 26)
+	for _, ch := range []byte(magazine) {
+		memory[ch-'a']++
+	}
+	for _, ch := range []byte(ransomNote) {
+		memory[ch-'a']--
+		if memory[ch-'a'] < 0 {
+			return false
 		}
 	}
 	return true
