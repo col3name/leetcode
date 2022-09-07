d0c4474725f963c7bd5976221b3130b6c89b4c79
mikha <ya.mikushoff@gmail.com>
2022-09-07 04:16:58 +0300

    feat: implement 606. Construct String from Binary Tree

---
+++ cmd/tree/bt/606/606.go
@@ -0,0 +1,39 @@
+package _606
+
+import (
+	"strconv"
+	"strings"
+)
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func tree2str(root *TreeNode) string {
+	var result strings.Builder
+
+	var helper func(node *TreeNode)
+	helper = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		result.WriteString(strconv.Itoa(node.Val))
+
+		if node.Left == nil && node.Right != nil {
+			result.WriteString("()")
+		} else if node.Left != nil {
+			result.WriteString("(")
+			helper(node.Left)
+			result.WriteString(")")
+		}
+		if node.Right != nil {
+			result.WriteString("(")
+			helper(node.Right)
+			result.WriteString(")")
+		}
+	}
+	helper(root)
+	return result.String()
+}
