f00aa101d9d55f9bf7885a277e4939dab5af9c0f
mikha <ya.mikushoff@gmail.com>
2022-09-11 09:20:16 +0300

    feat: try implement 1383. Maximum Performance of a Team

---
+++ cmd/heap/1383/1383.go
@@ -0,0 +1,66 @@
+package _1383
+
+import (
+	"container/heap"
+	"fmt"
+	"math"
+	"sort"
+)
+
+type minHeap []int
+
+func (h minHeap) Len() int {
+	return len(h)
+}
+
+func (h minHeap) First() interface{} {
+	return h[0]
+}
+func (h minHeap) Less(i int, j int) bool {
+	return h[i] > h[j]
+}
+
+func (h minHeap) Swap(i int, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *minHeap) Pop() interface{} {
+	a := *h
+	res := a[len(a)-1]
+	*h = a[:len(a)-1]
+	return res
+}
+
+func (h *minHeap) Push(i interface{}) {
+	*h = append(*h, i.(int))
+}
+
+func maxPerformance(n int, speeds []int, efficiencies []int, k int) int {
+	engineers := make([][2]int, n)
+
+	for i, speed := range speeds {
+		engineers[i] = [2]int{efficiencies[i], speed}
+	}
+
+	sort.Slice(&engineers, func(i, j int) bool {
+		return engineers[i][0] > engineers[j][0]
+	})
+
+	fmt.Println(engineers)
+	pq := minHeap{}
+	heap.Init(&pq)
+	var totalSpeed int
+	result := math.MinInt32
+	for _, engineer := range engineers {
+		heap.Push(&pq, engineer[1])
+		totalSpeed += engineer[1]
+		if pq.Len() > k {
+			totalSpeed -= heap.Pop(&pq).(int)
+		}
+		efficiency := engineer[0]
+		if totalSpeed*efficiency > totalSpeed {
+			result = totalSpeed * efficiency
+		}
+	}
+	return result % int(1e9+7)
+}
